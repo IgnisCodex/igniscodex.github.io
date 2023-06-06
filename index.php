ee75b98847ecfe61395bb2bdc062113fa7e1445e
IgnisCodex <edwardgwilletts23@icloud.com>
2023-06-06 15:51:46 +0100

    Uploading Test Files

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Penwith - Orders</title>
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="main_grid">
+            <div class="main_grid_topBar">
+                <a>Test</a>
+            </div>
+
+            <div class="main_grid_sideBar">
+                <h3>Welcome</h3>
+            </div>
+
+            <div class="main_grid_content">
+                
+                <h3>Welcome</h3>
+                <p>Lorem ipsum dolor sit amet, <span>consectetur</span> adipiscing elit. Donec tempus lacus sit amet urna consectetur, ut imperdiet erat tempus. Vivamus facilisis rutrum lorem, non congue ex tincidunt quis. Vestibulum tristique nunc dolor, accumsan iaculis nisi fermentum vitae. Maecenas condimentum sollicitudin viverra. Nulla aliquam, purus vitae venenatis interdum, felis sapien rhoncus ligula, non placerat ligula orci sit amet dui. Sed non dui venenatis, maximus orci et, sollicitudin diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur pharetra leo augue, et fringilla nisl mollis nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur tincidunt scelerisque augue, id accumsan lacus facilisis sit amet. Pellentesque sed placerat est. Etiam sed tempor justo. Sed auctor, nisi ut hendrerit venenatis, mauris nulla mattis ipsum, vitae dictum quam justo aliquet elit. Aenean cursus nunc quis nisi mollis, sit amet tincidunt orci faucibus.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempus lacus sit amet urna consectetur, ut imperdiet erat tempus. Vivamus facilisis rutrum lorem, non congue ex tincidunt quis. Vestibulum tristique nunc dolor, accumsan iaculis nisi fermentum vitae. Maecenas condimentum sollicitudin viverra. Nulla aliquam, purus vitae venenatis interdum, felis sapien rhoncus ligula, non placerat ligula orci sit amet dui. Sed non dui venenatis, maximus orci et, sollicitudin diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur pharetra leo augue, et fringilla nisl mollis nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur tincidunt scelerisque augue, id accumsan lacus facilisis sit amet. Pellentesque sed placerat est. Etiam sed tempor justo. Sed auctor, nisi ut hendrerit venenatis, mauris nulla mattis ipsum, vitae dictum quam justo aliquet elit. Aenean cursus nunc quis nisi mollis, sit amet tincidunt orci faucibus.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempus lacus sit amet urna consectetur, ut imperdiet erat tempus. Vivamus facilisis rutrum lorem, non congue ex tincidunt quis. Vestibulum tristique nunc dolor, accumsan iaculis nisi fermentum vitae. Maecenas condimentum sollicitudin viverra. Nulla aliquam, purus vitae venenatis interdum, felis sapien rhoncus ligula, non placerat ligula orci sit amet dui. Sed non dui venenatis, maximus orci et, sollicitudin diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur pharetra leo augue, et fringilla nisl mollis nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur tincidunt scelerisque augue, id accumsan lacus facilisis sit amet. Pellentesque sed placerat est. Etiam sed tempor justo. Sed auctor, nisi ut hendrerit venenatis, mauris nulla mattis ipsum, vitae dictum quam justo aliquet elit. Aenean cursus nunc quis nisi mollis, sit amet tincidunt orci faucibus.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempus lacus sit amet urna consectetur, ut imperdiet erat tempus. Vivamus facilisis rutrum lorem, non congue ex tincidunt quis. Vestibulum tristique nunc dolor, accumsan iaculis nisi fermentum vitae. Maecenas condimentum sollicitudin viverra. Nulla aliquam, purus vitae venenatis interdum, felis sapien rhoncus ligula, non placerat ligula orci sit amet dui. Sed non dui venenatis, maximus orci et, sollicitudin diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur pharetra leo augue, et fringilla nisl mollis nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur tincidunt scelerisque augue, id accumsan lacus facilisis sit amet. Pellentesque sed placerat est. Etiam sed tempor justo. Sed auctor, nisi ut hendrerit venenatis, mauris nulla mattis ipsum, vitae dictum quam justo aliquet elit. Aenean cursus nunc quis nisi mollis, sit amet tincidunt orci faucibus.</p>
+            </div>
+        </div>
+    </body>
+</html>
